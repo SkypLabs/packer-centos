562e1243ce7b726458ae299fa79cc107f3f967ab
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-08-01 16:40:56 +0200

    add 'vmtools.sh' script

---
+++ scripts/vmtools.sh
@@ -0,0 +1,18 @@
+# Install dependencies
+yum install -y gcc make perl kernel-devel-$(uname -r)
+
+# Mount Guest Additions ISO file
+mount -t iso9660 -o loop /root/VBoxGuestAdditions.iso /mnt
+
+# Execute the installer
+/mnt/VBoxLinuxAdditions.run
+
+# Display installation logs before deleting them
+cat /var/log/vboxadd-install.log
+rm -f /var/log/vboxadd-install.log
+
+# Unmount ISO file
+umount /mnt
+
+# Delete ISO file
+rm -f /root/VBoxGuestAdditions.iso
