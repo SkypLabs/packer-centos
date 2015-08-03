a8ee1c161380bc34c0632c42f35654028f75eb7b
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-08-04 01:09:37 +0200

    add comments in 'vagrant.sh' script

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,24 +1,32 @@
 if [ "$VAGRANT" == "true" ]
 then
+	# Add vagrant user
 	useradd vagrant
 
+	# Do not require a password with sudo
+	# for vagrant user
 	cat > /etc/sudoers.d/vagrant <<'EOF'
 Defaults:vagrant !requiretty
 Defaults:vagrant secure_path=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin
 vagrant ALL=(ALL) NOPASSWD: ALL
 EOF
 
+	# Change permissions on vagrant configuration file for sudo
 	chmod 0440 /etc/sudoers.d/vagrant
 
+	# Add SSH directory for vagrant user
 	install -d -o vagrant -g vagrant -m 0700 /home/vagrant/.ssh
 
+	# Download the insecure public key from GitHub official repository
 	curl \
 		--location \
 		--output /home/vagrant/.ssh/authorized_keys \
 		https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub
 
+	# Change owner and group on SSH authorized keys file for vagrant user
 	chown vagrant:vagrant /home/vagrant/.ssh/authorized_keys
 
+	# Change password for root and vagrant users
 	echo 'root:vagrant' | chpasswd
 	echo 'vagrant:vagrant' | chpasswd
 fi
